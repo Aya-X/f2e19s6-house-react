0aeb7e13aac687bac724c35e9e19eca289bea8b4
Aya <90342033+Aya-X@users.noreply.github.com>
2023-11-09 15:53:03 +0800

    feat: Add BasicModal component

--- src/routes/room.tsx
+++ src/routes/room.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+
+import BasicModal from '../components/BasicModal';
 
 function Room(): JSX.Element {
   const { roomId } = useParams();
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
       <h1>ROOM</h1>
+
+      <button type="button" onClick={handleOpen}>
+        BOOKING
+      </button>
+
+      <BasicModal open={open} handleClose={handleClose}>
+        <span>BOOKING</span>
+      </BasicModal>
+
       <p>
         roomId:<code> {roomId}</code>
       </p>
